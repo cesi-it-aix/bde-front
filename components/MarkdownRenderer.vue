f528d339d1e84725a18a4a9524af8e12036c903d
Luc <luc.vandevelde@viacesi.fr>
2019-12-02 13:25:28 +0100

    add markdown component

---
+++ components/MarkdownRenderer.vue
@@ -0,0 +1,16 @@
+<template>
+  <div v-html="$md.render(model)"></div>
+</template>
+
+<script>
+// ajouter une query que l'on donnera a markdown
+var mardown = '# Hello World!'
+
+export default {
+  data() {
+    return {
+      model: mardown
+    }
+  }
+}
+</script>
