0498408b1da3922ed40d13d5924d25f52568061d
Hougo <hougo13@gmail.com>
2020-01-20 19:31:35 +0100

    EventCard #2mk3xj

---
+++ components/EventCard.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-card class="event-card w ma-5" :to="uri" nuxt flat>
+    <div class="d-flex">
+      <day-card :date="event.dateStart" />
+      <v-list-item two-line class="tex">
+        <v-list-item-content>
+          <v-list-item-title class="headline mb-1">{{event.title}}</v-list-item-title>
+          <v-list-item-subtitle>{{event.summary}}</v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+    </div>
+  </v-card>
+</template>
+
+<script>
+import DayCard from '~/components/DayCard.vue';
+
+export default {
+  components: {
+    DayCard
+  },
+  props: {
+    event: Object
+  },
+  computed: {
+    uri() {
+      return `/events/${this.event.id}`;
+    }
+  }
+};
+</script>
+
+<style>
+.event-card {
+  width: 100%;
+}
+</style>
